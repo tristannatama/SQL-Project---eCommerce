14b34cef80d42df02cb3dc4fe4886b0c091444a7
Tristan <tristantampu7@gmail.com>
2022-08-03 13:12:14 +0700

    queries for metrics before combining

--- customers.sql
+++ customers.sql
@@ -12,13 +12,31 @@ from
 group by 1,2
 ;
 
--- select date (order_purchase_timestamp) 
--- from orders
--- order by date;
+select to_char(order_purchase_timestamp, 'yyyy-mm') as bulan,
+       count(*) filter (where new_customer) as new_customers
+from (       
+  select customer_id,
+         order_purchase_timestamp, 
+         order_purchase_timestamp = min(order_purchase_timestamp) over w as new_customer
+  from orders
+  window w as (partition by customer_id)
+) tmp
+group by to_char(order_purchase_timestamp, 'yyyy-mm')
+order by to_char(order_purchase_timestamp, 'yyyy-mm');
 
--- select 
--- extract(year from(order_purchase_timestamp))as years, count(extract(year from(order_purchase_timestamp)))
--- from orders
--- group by 1
+select customer_id, count(customer_id)
+from orders
+group by 1
+having count(customer_id) > 1;
+
+with mp as(
+select 
+	extract(year from(order_purchase_timestamp))as tahun, 
+	count(order_purchase_timestamp) as total_orders
+from orders 
+group by 1)
+select round(avg(total_orders), 0) from mp
+
+
 
 
